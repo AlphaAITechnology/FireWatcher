f15fd7a9a26ef2234c63d6d4af07c8bf5518975b
Uzair05 <ubcasim@gmail.com>
2024-12-02 15:36:50 +0800

    Change GPU to cuda:1

--- application.py
+++ application.py
@@ -151,7 +151,7 @@ def FireAnalysis():
             while not capture_images_f.empty():
                 camera_TID, img = capture_images_f.get()
                 
-                results = model(img, stream=True, conf=minimum_confidence, classes=[1]) # all classes for fire 0: smoke, 1: fire
+                results = model(img, stream=True, conf=minimum_confidence, classes=[0], device='cuda:1') # all classes for fire 0: smoke, 1: fire
                 results = [np.floor(result.boxes.xyxy.cpu().numpy()) for result in results][0]
                 
                 
@@ -188,7 +188,7 @@ def HumanAnalysis():
         try:
             while not capture_images_q.empty():
                 camera_TID, img = capture_images_q.get()
-                results = model(img, stream=True, conf=minimum_confidence, classes=[0]) # only person class
+                results = model(img, stream=True, conf=minimum_confidence, classes=[0], device='cuda:1') # only person class
                 results = [np.floor(result.boxes.xyxy.cpu().numpy()) for result in results] # bring to xyxy numpy
                 
                 # get max roi intersection of each detection
