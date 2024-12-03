03f4def617835ef9622506763a3308c71d547a4f
Uzair05 <ubcasim@gmail.com>
2024-12-03 10:43:14 +0800

    Added Loggings

--- application.py
+++ application.py
@@ -16,6 +16,10 @@ import logging
 
 
 def ImageSending_IO():
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(filename='./LOGS/FILES/ImageSending.logs', encoding='utf-8', level=logging.INFO)
+
+
     base_url = "https://fire-api.alphaaitech.com"
     x_api_token = None
     with open("./.env", 'r') as env_file:
@@ -26,12 +30,16 @@ def ImageSending_IO():
     print("x_api_token loaded from .env file")
 
     while elegant_shutdown.empty():
+        dtm_ = datetime.datetime.now(pytz.utc).isoformat().split('+')[0]
+
         # Handle Fire Detection 
         while(not sending_images_f.empty()):
             img_path = sending_images_f.get()
             camera_tstamp, camera_id = os.path.basename(img_path).split('@')
             camera_tstamp = camera_tstamp.split('_')[1]
             camera_id = camera_id.split('.')[0]
+            
+            logger.info(f"Uploading Image:\t{img_path}")
 
             with open(img_path, "rb") as files_:
                 # storing file
@@ -40,8 +48,12 @@ def ImageSending_IO():
                     files={'file': (img_path, files_, 'image/webp')},
                     headers={"x-api-token": x_api_token},
                 )
+            
+            logger.info(f"Uploading Image Response:\t{file_upload_response.status_code},\t{json.dumps(file_upload_response)}")
+
             if (file_upload_response.status_code == 201):
                 upload_response = json.loads(file_upload_response.text)
+                logger.info(f"Uploading URL:\t{upload_response["fileUrl"]}")
                 alert_response = req.post(
                         f"{base_url}/alert-record",
                         headers={"x-api-token": x_api_token},
@@ -52,6 +64,10 @@ def ImageSending_IO():
                             "alertAt": camera_tstamp
                         }
                     )
+                logger.info(f"Uploading URL Response:\t{alert_response.status_code},\t{json.dumps(alert_response)}")
+
+
+
                 
                 if (alert_response.status_code >= 200 or alert_response.status_code <= 203):
                     print(alert_response.text)
@@ -106,6 +122,9 @@ def ImageSending_IO():
 
 
 def ImageSaving_IO():
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(filename='./LOGS/FILES/ImageSaving.logs', encoding='utf-8', level=logging.INFO)
+
     if os.path.exists("./saved_images"):
         if not os.path.isdir("./saved_images"):
             os.remove("./saved_images")
@@ -114,11 +133,14 @@ def ImageSaving_IO():
         os.mkdir("./saved_images")
     
     while elegant_shutdown.empty():
+        dtm_ = datetime.datetime.now(pytz.utc).isoformat().split('+')[0]
         while(not printing_images_f.empty()):
             camera_TID, img = printing_images_f.get()
             img_path = f"./saved_images/f_{camera_TID}.webp"
             cv.imwrite(img_path, img)
             sending_images_f.put(img_path)
+
+            logger.info(f"Saved Fire Image:{dtm_}")
             
             del img
             del camera_TID
@@ -128,6 +150,8 @@ def ImageSaving_IO():
             img_path = f"./saved_images/{camera_TID}.webp"
             cv.imwrite(img_path, img)
             sending_images_q.put(img_path)
+
+            logger.info(f"Saved Human Image:{dtm_}")
             
             del img
             del camera_TID
@@ -137,6 +161,8 @@ def ImageSaving_IO():
 
 
 def FireAnalysis():
+    logger = logging.getLogger(__name__)
+    logging.basicConfig(filename='./LOGS/FILES/FireAnalysis.logs', encoding='utf-8', level=logging.INFO)
 
     model = YOLO("Weights/fire_v8l.pt")
     print("Fire Model Loaded")
@@ -151,22 +177,30 @@ def FireAnalysis():
     while elegant_shutdown.empty():
         try:
             while not capture_images_f.empty():
+                dtm_ = datetime.datetime.now(pytz.utc).isoformat().split('+')[0]
                 camera_TID, img = capture_images_f.get()
                 
                 results = model(img, stream=True, conf=minimum_confidence, classes=[0], device='cuda:1', verbose=False) # all classes for fire 0: smoke, 1: fire
                 results = [np.floor(result.boxes.xyxy.cpu().numpy()) for result in results][0]
                 
-                
-                dec_window_list_results.append(results) ## list of numpy # store image and model results 
+                resulting_flag = 1 if results.shape[0]>0 else 0
+                dec_window_list_results.append(resulting_flag) ## list of int flags; storing results
+
+                if (resulting_flag==1):
+                    logger.info(f"Fire Detected:\t{dtm_}")
+
                 while (len(dec_window_list_results)>dec_window_size): # only stores static number of images
                     dec_window_list_results.pop(0) # remove oldest image
 
 
-                if (not seen_before) and (sum([(1 if res.shape[0]>0 else 0) for res in dec_window_list_results]) >= dec_window_approv): # we have 5+ out of 50 positives
+                if (not seen_before) and (sum(dec_window_list_results) >= dec_window_approv): # we have 5+ out of 50 positives
                     printing_images_f.put((camera_TID, img))
                     seen_before = True
-                elif (seen_before) and (sum([(1 if res.shape[0]>0 else 0) for res in dec_window_list_results]) <= dec_window_release):
+                    logger.info("Fire Alert sent; deactivating alert")
+                elif (seen_before) and (sum(dec_window_list_results) <= dec_window_release):
                     seen_before = False
+                    logger.info("Fire Extinguished; deactivating alert")
+
 
 
                 
@@ -180,7 +214,7 @@ def FireAnalysis():
 
 def HumanAnalysis():
     logger = logging.getLogger(__name__)
-    logging.basicConfig(filename='HumanAnalysis.logs', encoding='utf-8', level=logging.INFO)
+    logging.basicConfig(filename='./LOGS/FILES/HumanAnalysis.logs', encoding='utf-8', level=logging.INFO)
 
     model = YOLO("Weights/yolov8l.pt") 
     print("Human Model Loaded")
@@ -243,7 +277,7 @@ def HumanAnalysis():
                         imgr, _ = max(dec_window_list_imgresults, key=lambda x: x[1])
                         # Send image for printing
                         if imgr is not None: # safety --> will only be an issue if `dec_window_approv==0`
-                            logger.info("Human alert sent; Alert deactivated.")
+                            logger.info("Human Alert sent; Alert deactivated.")
                             printing_images_q.put((camera_TID, imgr)) 
                             has_seen = True
                 else: # if an old detection has been sent
@@ -262,13 +296,9 @@ def HumanAnalysis():
     elegant_shutdown.put(True)
 
 
-
-
-
-
 def ImageCapture_IO():
     logger = logging.getLogger(__name__)
-    logging.basicConfig(filename='ImageCapture.logs', encoding='utf-8', level=logging.INFO)
+    logging.basicConfig(filename='./LOGS/FILES/ImageCapture.logs', encoding='utf-8', level=logging.INFO)
 
     cameras_link = None
     cameras_id = None
@@ -330,9 +360,6 @@ def ImageCapture_IO():
 
 
 def main():
-    # logger = logging.getLogger(__name__)
-    # logging.basicConfig(filename='main.logs', encoding='utf-8', level=logging.INFO)
-
 
     if not(os.path.exists("./LOGS/") and os.path.isdir("./LOGS/")):
         os.mkdir("./LOGS/")
@@ -340,6 +367,8 @@ def main():
         os.mkdir("./LOGS/HUMAN/")
     if not(os.path.exists("./LOGS/FIRE/") and os.path.isdir("./LOGS/FIRE/")):
         os.mkdir("./LOGS/FIRE/")
+    if not(os.path.exists("./LOGS/FILES/") and os.path.isdir("./LOGS/FILES/")):
+        os.mkdir("./LOGS/FILES/")
 
 
     parser = argparse.ArgumentParser(description='Watch Cameras for Humans')
